346a14b7de05e3569fa73f80b914281ffc682859
ashwinjo <ashwin.joshi@keysight.com>
2022-12-16 15:27:19 -0800

    Testing with TestPlatform construct

--- sample.py
+++ sample.py
@@ -1,30 +1,32 @@
-from ixnetwork_restpy import SessionAssistant
-from typing import List
+from ixnetwork_restpy.testplatform.testplatform import TestPlatform # ixnetwork_restpy==1.0.45
 import time
-
+from typing import List
 
 class TestL1Settings(object):
 
-    def __init__(self, ipaddr, user, password, session_id, clear_config):
-        """_summary_
+    def __init__(self, ipaddr, user, password, session_name=None):
+        """Constructor for TestCase CLasses
 
         Args:
-            ipaddr (_type_): _description_
-            user (_type_): _description_
-            password (_type_): _description_
-            session_id (_type_): _description_
-            clear_config (_type_): _description_
+            ipaddr (_type_): ip of ixia chasses
+            user (_type_): username for ixia chassis
+            password (_type_): password for ixia chassis
+            session_name (_type_): session name on Ixia Linux chassis to connect to
         """
-        session_assistant = SessionAssistant(IpAddress=ipaddr, UserName=user, Password=password,
-                                             LogLevel=SessionAssistant.LOGLEVEL_INFO,
-                                             ClearConfig=clear_config,
-                                             SessionId=session_id)
-
+        testPlatform = TestPlatform(ip_address=ipaddr, log_file_name='restpy.log')
+        testPlatform.Trace = 'request_response'
+        testPlatform.Authenticate(user, password)
+        
+        # Multiple sessions possible only with Linux Chassis
+        if not session_name:
+            session_assistant = testPlatform.Sessions.find()
+        else:
+            session_assistant = testPlatform.Sessions.find(Name=session_name)
         self.ixnetwork = session_assistant.Ixnetwork
     
 
     def vport_link_up_down(self, **kwargs):
-        """_summary_
+        """Turn L1 Ports UP/Down
 
         Returns:
             _type_: _description_
@@ -34,14 +36,15 @@ class TestL1Settings(object):
         operation =  kwargs.get("operation")
         for vport_name in vports:
             vport = self.ixnetwork.Vport.find(Name=vport_name)
+            
             vport.LinkUpDn(Arg2=operation)
             res = res + f"{vport_name} turned {operation}\n"
         print(res)
 
     def vport_laser_on_off(self, **kwargs):
-        """_summary_
+        """Turn Laser On Off
 
-        Returns:
+        Returns:ß
             _type_: _description_
         """
         res = ""
@@ -59,7 +62,7 @@ class TestL1Settings(object):
         print(res)
 
     def vport_insert_local_fault(self, **kwargs):
-        """_summary_
+        """Insert L1 local/remote fauls and then initiate their transmission
         """
         vports = kwargs.get("vports")
         source_value =  kwargs.get("source_value")
@@ -68,7 +71,7 @@ class TestL1Settings(object):
         
         for vport_name in vports:
             vport = self.ixnetwork.Vport.find(Name=vport_name)
-            vp = vport.L1Config.find().NovusHundredGigLan.find()
+            vp = vport.L1Config.NovusHundredGigLan
 
             # localFault | remoteFault
             vp.TypeAOrderedSets = source_value
@@ -82,38 +85,36 @@ class TestL1Settings(object):
             
 
     def vport_increment_decrement_frequency(self, vports: List, operation: str, step_size: int):
-        """_summary_
+        """Increment Decrement Transmit deviation
 
         Args:
-            vports (List): _description_
-            operation (str): _description_
-            step123 (int): _description_
+            vports (List): Ports to apply changes on
+            operation (str): increment/decrement frequency
+            step_size (int): incement/decrement by what value
         """
         for vport_name in vports:
             vport = self.ixnetwork.Vport.find(Name=vport_name)
 
-            vport.L1Config.find().NovusHundredGigLan.find().EnablePPM = True
+            vport.L1Config.NovusHundredGigLan.EnablePPM = True
             print(
                 f"PPM Enabled: {vport.L1Config.NovusHundredGigLan.EnablePPM}")
 
             print(vport.L1Config.NovusHundredGigLan.Ppm)
             if operation == "increment":
-                vport.L1Config.find().NovusHundredGigLan.find().Ppm = step_size
+                vport.L1Config.NovusHundredGigLan.Ppm = step_size
                 print(f"New Value: {vport.L1Config.NovusHundredGigLan.Ppm}")
 
             elif operation == "decrement":
-                vport.L1Config.find().NovusHundredGigLan.find(
-                ).Ppm = f"-{step_size}"
+                vport.L1Config.NovusHundredGigLan.Ppm = f"-{step_size}"
                 print(f"New Value: {vport.L1Config.NovusHundredGigLan.Ppm}")
 
-            vport.L1Config.find().NovusHundredGigLan.find().EnablePPM
-            import time
-            time.sleep(10)
+            vport.L1Config.NovusHundredGigLan.EnablePPM
+            time.sleep(5)
             print(
                 f"PPM Enabled: {vport.L1Config.NovusHundredGigLan.EnablePPM}")
 
     def vport_clock_source_faults(self, **kwargs):
-        """_summary_
+        """Inserting clock faults bases on loopback modes
         """
         vports = kwargs.get("vports")
         loopback_mode =  kwargs.get("loopback_mode")
@@ -122,12 +123,12 @@ class TestL1Settings(object):
             vport = self.ixnetwork.Vport.find(Name=vport_name)
             print(
                 f"Loopback mode: {vport.L1Config.NovusHundredGigLan.LoopbackMode}")
-            vport.L1Config.find().NovusHundredGigLan.find().LoopbackMode = loopback_mode
+            vport.L1Config.NovusHundredGigLan.LoopbackMode = loopback_mode
             print(
                 f"Loopback mode: {vport.L1Config.NovusHundredGigLan.LoopbackMode}")
 
     def vport_send_undersize_packets(self, **kwargs):
-        """_summary_
+        """Sending frames with modified attributes for frame size, crc
         """
         create_traffic_item = kwargs.get("create_traffic_item", False)
         traffic_item_name = kwargs.get("traffic_item_name", "TI")
@@ -162,20 +163,14 @@ class TestL1Settings(object):
 
             if runt or crc:
                 configElement.FrameSize.FixedSize = desired_frame_size
-                configElement.Crc = 'badCrc'
+                configElement.Crc = 'goodCrc'
                 
                 print(configElement)
-    
-    def reboot(self):
-        vport = self.ixnetwork.Vport.find(Name="Port_1")
-        vport.ResetPortCpu(async_operation=True)
-                
 
 
 if __name__ == "__main__":
     tl1s = TestL1Settings(ipaddr='10.36.236.121', user='admin',
-                          password='Kimchi123Kimchi123!', session_id='12', 
-                          clear_config=False)
+                          password='Kimchi123Kimchi123!', session_name="IxNetwork Test 12")
 
        
     def link_up_down(vports, wait_interval, repetition):
@@ -234,17 +229,184 @@ if __name__ == "__main__":
         
     
         
-     
-    link_up_down(vports=["Port_1"], wait_interval=5, repetition=2)
+    print("==Link Up Down==")
+    link_up_down(vports=["Port_1"], wait_interval=1, repetition=2)
+    
+    print("==Laser On Off==")
     laser_on_off(vports=["Port_1"], wait_interval=5, repetition=2)
-    generate_faults(vports=["Port_1"], wait_interval=5, repetition=2)
+    
+    print("==Generating Faults==")
+    generate_faults(vports=["Port_1"], wait_interval=5, repetition=1)
+    
+    print("==Generating Clock Faults==")
     vport_clock_source_faults_toggle(vports=["Port_1"], wait_interval=5, repetition=2)
+    
+    print("==Increment Decrement Transmit Deviation by PPM values==")
     vport_increment_decrement_frequency(vports=["Port_1"], wait_interval=5, repetition=2)
     
+    print("==Generating undersize packets==")
     generate_undersize_packets_traffic(wait_interval=5, repetition=2)
+    
+    print("==Generating RUNT packets==")
     generate_runt_traffic(wait_interval=5, repetition=2)
+    
+    print("==Generating packets with CRC errors=")
     generate_crc_error_traffic(wait_interval=5, repetition=2)
     
     
-    
+    """Sample Output:
+       ---------------
+        (ixn-venv) ashwjosh@C0HD4NKHCX IxSampleScripts % python sample.py  
+        ==Link Up Down==
+        Port_1 turned down
+
+        Port_1 turned up
+
+        Port_1 turned down
+
+        Port_1 turned up
+
+        ==Laser On Off==
+        Port_1 turned down
+
+        Port_1 turned up
+
+        Port_1 turned down
+
+        Port_1 turned up
+
+        ==Generating Faults==
+        NovusHundredGigLan[0]: /api/v1/sessions/12/ixnetwork/vport/1/l1Config/novusHundredGigLan
+                AutoInstrumentation: endOfFrame
+                BadBlocksNumber: 4
+                EnableAutoNegotiation: False
+                EnablePPM: True
+                EnableRsFec: True
+                EnableRsFecStats: True
+                EnabledFlowControl: True
+                FirecodeAdvertise: False
+                FirecodeForceOff: False
+                FirecodeForceOn: False
+                FirecodeRequest: False
+                FlowControlDirectedAddress: 01 80 C2 00 00 01
+                ForceDisableFEC: False
+                GoodBlocksNumber: 0
+                IeeeL1Defaults: False
+                LaserOn: True
+                LinkTraining: False
+                LoopContinuously: True
+                LoopCountNumber: 1
+                Loopback: True
+                LoopbackMode: internalLoopback
+                Ppm: 70
+                RsFecAdvertise: False
+                RsFecForceOn: False
+                RsFecRequest: False
+                SendSetsMode: typeAOnly
+                Speed: speed100g
+                StartErrorInsertion: True
+                TxIgnoreRxLinkFaults: False
+                TypeAOrderedSets: localFault
+                TypeBOrderedSets: remoteFault
+                UseANResults: False
+        NovusHundredGigLan[0]: /api/v1/sessions/12/ixnetwork/vport/1/l1Config/novusHundredGigLan
+                AutoInstrumentation: endOfFrame
+                BadBlocksNumber: 4
+                EnableAutoNegotiation: False
+                EnablePPM: True
+                EnableRsFec: True
+                EnableRsFecStats: True
+                EnabledFlowControl: True
+                FirecodeAdvertise: False
+                FirecodeForceOff: False
+                FirecodeForceOn: False
+                FirecodeRequest: False
+                FlowControlDirectedAddress: 01 80 C2 00 00 01
+                ForceDisableFEC: False
+                GoodBlocksNumber: 0
+                IeeeL1Defaults: False
+                LaserOn: True
+                LinkTraining: False
+                LoopContinuously: True
+                LoopCountNumber: 1
+                Loopback: True
+                LoopbackMode: internalLoopback
+                Ppm: 70
+                RsFecAdvertise: False
+                RsFecForceOn: False
+                RsFecRequest: False
+                SendSetsMode: typeBOnly
+                Speed: speed100g
+                StartErrorInsertion: True
+                TxIgnoreRxLinkFaults: False
+                TypeAOrderedSets: localFault
+                TypeBOrderedSets: remoteFault
+                UseANResults: False
+        ==Generating Clock Faults==
+        Loopback mode: internalLoopback
+        Loopback mode: none
+        Loopback mode: none
+        Loopback mode: lineLoopback
+        Loopback mode: lineLoopback
+        Loopback mode: internalLoopback
+        Loopback mode: internalLoopback
+        Loopback mode: none
+        Loopback mode: none
+        Loopback mode: lineLoopback
+        Loopback mode: lineLoopback
+        Loopback mode: internalLoopback
+        ==Increment Decrement Transmit Deviation by PPM values==
+        PPM Enabled: True
+        70
+        New Value: 70
+        PPM Enabled: True
+        PPM Enabled: True
+        70
+        New Value: 70
+        PPM Enabled: True
+        PPM Enabled: True
+        70
+        New Value: 70
+        PPM Enabled: True
+        PPM Enabled: True
+        70
+        New Value: 70
+        PPM Enabled: True
+        ==Generating undersize packets==
+        ==Generating RUNT packets==
+        ConfigElement[0]: /api/v1/sessions/12/ixnetwork/traffic/trafficItem/8/configElement/1
+                Crc: goodCrc
+                DestinationMacMode: manual
+                EnableDisparityError: False
+                EncapsulationName: Ethernet.VLAN.IPv4
+                EndpointSetId: 1
+                PreambleCustomSize: 8
+                PreambleFrameSizeMode: auto
+        ConfigElement[0]: /api/v1/sessions/12/ixnetwork/traffic/trafficItem/8/configElement/1
+                Crc: goodCrc
+                DestinationMacMode: manual
+                EnableDisparityError: False
+                EncapsulationName: Ethernet.VLAN.IPv4
+                EndpointSetId: 1
+                PreambleCustomSize: 8
+                PreambleFrameSizeMode: auto
+        ==Generating packets with CRC errors=
+        ConfigElement[0]: /api/v1/sessions/12/ixnetwork/traffic/trafficItem/8/configElement/1
+                Crc: goodCrc
+                DestinationMacMode: manual
+                EnableDisparityError: False
+                EncapsulationName: Ethernet.VLAN.IPv4
+                EndpointSetId: 1
+                PreambleCustomSize: 8
+                PreambleFrameSizeMode: auto
+        ConfigElement[0]: /api/v1/sessions/12/ixnetwork/traffic/trafficItem/8/configElement/1
+                Crc: goodCrc
+                DestinationMacMode: manual
+                EnableDisparityError: False
+                EncapsulationName: Ethernet.VLAN.IPv4
+                EndpointSetId: 1
+                PreambleCustomSize: 8
+                PreambleFrameSizeMode: auto
+        (ixn-venv) ashwjosh@C0HD4NKHCX IxSampleScripts % 
+        """
     
