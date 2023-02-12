613a8dd9f1860571c663e709f7c4da8da4ace2e3
Ashwin Joshi <120066169+ashwinjo@users.noreply.github.com>
2023-02-11 22:08:10 -0800

    Formatting linux ixos code

--- IxOS/Utilities/Python/Utilities/Python/chassis_hw_info_linux_cli.py
+++ IxOS/Utilities/Python/Utilities/Python/chassis_hw_info_linux_cli.py
@@ -1,6 +1,7 @@
 import paramiko
+import time
 
-def get_linux_chassis_information():
+def get_linux_chassis_information(chassis_ip, username, password):
     # Create an instance of the SSH client
     ssh = paramiko.SSHClient()
 
@@ -8,7 +9,7 @@ def get_linux_chassis_information():
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     # Connect to the server
-    ssh.connect("10.36.236.121", username="admin", password="xxxxxx")
+    ssh.connect(chassis_ip, username=username, password=password)
     chan = ssh.invoke_shell(width=500)
 
     # Ssh and wait for the password prompt.
@@ -17,15 +18,15 @@ def get_linux_chassis_information():
     while not resp.endswith('# '):
         resp = chan.recv(9999)
         resp = str(resp, 'UTF-8')
-    
-    print("\n******************* License Details ***********************\n")
-    chan.send('show licenses\n')
+        
+    print("\n******************* Chassis Info Fetch Date ***********************\n")
+    chan.send('show date\n')
     resp  = ''
     while not resp.endswith('# '):
         resp = chan.recv(9999)
         resp = str(resp, 'UTF-8')
         print(resp)
-        
+    
         
     print("\n******************* Chassis Uptime ***********************\n")
     chan.send('show uptime\n')
@@ -34,6 +35,7 @@ def get_linux_chassis_information():
         resp = chan.recv(9999)
         resp = str(resp, 'UTF-8')
         print(resp)
+    
         
     print("\n******************* Chassis Topology ***********************\n")
     chan.send('show topology\n')
@@ -43,8 +45,9 @@ def get_linux_chassis_information():
         resp = str(resp, 'UTF-8')
         print(resp)
     
-    print("\n******************* Chassis Info Fetch Date ***********************\n")
-    chan.send('show date\n')
+        
+    print("\n******************* License Details ***********************\n")
+    chan.send('show licenses\n')
     resp  = ''
     while not resp.endswith('# '):
         resp = chan.recv(9999)
@@ -55,4 +58,11 @@ def get_linux_chassis_information():
     ssh.close()
     
 
-get_linux_chassis_information()
+
+CHASSIS_LIST = [{"ip": "X.X.X.X", "username": "XXX", "password": "XXX"}]
+
+if __name__ == "__main__":
+    for chassis in CHASSIS_LIST:
+        get_linux_chassis_information(chassis["ip"], 
+                                      chassis["username"], 
+                                      chassis["password"])
